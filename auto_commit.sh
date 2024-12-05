7cd27385e049d5f277aeb011bd15d602d7af5383
Aldo <aldo_roman13@hotmail.com>
2024-12-05 10:54:51 -0600

    changes in code comments

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,27 +1,23 @@
-# Script Ajustado para Windows con Git Bash
+# Script Ajustado con Git Bash
 
 #!/bin/bash
 
 # Ruta al repositorio local
 REPO_PATH="D:/Proyects/git-automatize/Git-automatize"
 
-# Mensaje del commit
 COMMIT_MESSAGE="Commit automatic daily"
 
-# Fecha actual para añadir al commit
+# Fecha actual para añadir
 FECHA=$(date +"%Y-%m-%d")
 
 # Moverse al repositorio
 cd "$REPO_PATH" || exit
 
-# Crear o actualizar un archivo para que haya algo que commitear
+# Crear o actualizar un archivo
 echo "Actualizado el $FECHA" >> actualizacion_diaria.txt
 
-# Añadir cambios
 git add .
 
-# Hacer commit
 git commit -m "$COMMIT_MESSAGE - $FECHA"
 
-# Subir cambios
 git push
